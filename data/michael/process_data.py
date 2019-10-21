ca58ae1c8ab7a950a6d09f3e3bbd58d87151f83c
Michael Blackmon <differentiablef@gmail.com>
2019-10-21 15:18:16 -0400

    rough out of data processing code

--- data/michael/process_data.py
+++ data/michael/process_data.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.ext.declarative import declarative_base
 
 # standard
-import sys, os
+import sys, os, bz2, json
 
 # utility
 import pandas as pd
@@ -27,28 +27,65 @@ class NCCounty(Base):
     # determines whether a point is contained within the
     #   boundary associated with this county
     def in_boundary(self, point):
-        
+        for vertex in self.boundary:
+            print(vertex)
         return False
     pass
-    
+
+# class describing the table containing the
+#  county level lightening strike data
+class LighteningStrikes(Base):
+    __tablename__ = 'CountyStrikes'
+    id      = Column(Integer, primary_key=True)
+    county  = Column(Integer) # id of county 
+    strikes = Column(Integer) # number of lightening strikes in county
+    pass
+
 # methods ######################################################################
 
 def load_counties(session):
+    """ Load list of counties """
     objs = session.query(NCCounty).all()
     return objs    
 
-def load_data():
-    pass
+def load_data(path):
+    """ Load data from bzip'd raw json """
+    # will hold aggregated json
+    objs = []
 
-def generate_histogram():
-    pass
+    # open and process contents of bzip'd json file
+    #  combining all lines into a single list
+    with bz2.open(path, 'r') as ifile:
+        for line in ifile:
+            if len(line) > 1:
+                objs.extend(json.loads(line))
+
+    # convert "POINT" entries into tubles and rename
+    #  some attributes
+    for obj in objs:
+        s = obj.get('center_point').split(' ')
+        obj['point'] = (float(s[0][6:]), float(s[1][:-1]))
+        del obj['center_point']
+
+        obj['strikes'] = obj['number_of_strikes']
+        del obj['number_of_strikes']
+        
+    return objs
 
 def main():
+    # path to lightenting data
+    datafile = './raw-lightening.json.bz2'
+    
+    # setup connection to postgresql database
     engine  = create_engine("postgresql://postgres@localhost:5432/project3")
     conn    = engine.connect()
     session = Session(bind=engine)
 
+    # extract counties from database
     counties = load_counties(session)
+
+    # load data to be binned by county
+    data = load_data(datafile)
     pass
 
 # script entry-point ###########################################################
